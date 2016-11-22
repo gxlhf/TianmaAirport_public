9acf228489d76f522c50f426702c8de3b35bf37f
xxk1004 <374123548@qq.com>
2016-11-22 20:20:20 +0800

    entity edit.

--- flight_manage/src/com/entity/User.java
+++ flight_manage/src/com/entity/User.java
@@ -13,20 +13,65 @@ public class User {
 		}
 		else{
 			if(city!=null&&airline!=null){//查询目的地为该city，航空公司为该airline的离港航班信息
+				/*
+				 * DepartureFlightInfo[] departureFlightInfo=search1(String city,String airline); 
+				 * 数据库操作：查询 目的地为该city，航空公司为该airline的离港航班信息
+				 * 形参为目的地和航空公司，返回类型为DepartureFlightInfo对象数组
+				 */				
 				return departureFlightInfo;
 			}
 			if(city==null&&airline!=null){//查询航空公司为该airline的离港航班信息
+				/*
+				 * DepartureFlightInfo[] departureFlightInfo=search2(String airline); 
+				 * 数据库操作：查询航空公司为该airline的离港航班信息
+				 * 形参为航空公司，返回类型为DepartureFlightInfo对象数组
+				 */
 				return departureFlightInfo;
 			}
 			if(city!=null&&airline==null){//查询目的地为该city的离港航班信息
+				/*
+				 * DepartureFlightInfo[] departureFlightInfo=search3(String city); 
+				 * 数据库操作：查询目的地为该city的离港航班信息
+				 * 形参为目的地，返回类型为DepartureFlightInfo对象数组
+				 */
 				return departureFlightInfo;
 			}
 		}
 		return departureFlightInfo;
 	}
+	//到港航班信息查询函数，输入三个参数依次为出发地、航班号、航空公司，返回一个ArrivalFlightInfo对象数组
 	ArrivalFlightInfo[] searchArrivalFlightInfo(String city, String flightNumber, String airline)
 	{
-		ArrivalFlightInfo[] arrivalFlightInfos = null;
-		return arrivalFlightInfos;
+		ArrivalFlightInfo[] arrivalFlightInfo = null;
+		if(flightNumber!=null){//查询该航班号的到港航班信息
+			return arrivalFlightInfo;
+		}
+		else{
+			if(city!=null&&airline!=null){//查询出发地为该city，航空公司为该airline的到港航班信息
+				/*
+				 * ArrivalFlightInfo[] arrivalFlightInfo=search1(String city,String airline); 
+				 * 数据库操作：查询 出发地为该city，航空公司为该airline的到港航班信息
+				 * 形参为出发地和航空公司，返回类型为ArrivalFlightInfo对象数组
+				 */				
+				return arrivalFlightInfo;
+			}
+			if(city==null&&airline!=null){//查询航空公司为该airline的到港航班信息
+				/*
+				 * ArrivalFlightInfo[] arrivalFlightInfo=search2(String airline); 
+				 * 数据库操作：查询航空公司为该airline的到港航班信息
+				 * 形参为航空公司，返回类型为ArrivalFlightInfo对象数组
+				 */
+				return arrivalFlightInfo;
+			}
+			if(city!=null&&airline==null){//查询出发地为该city的到港航班信息
+				/*
+				 * ArrivalFlightInfo[] arrivalFlightInfo=search3(String city); 
+				 * 数据库操作：查询出发地为该city的到港航班信息
+				 * 形参为出发地，返回类型为ArrivalFlightInfo对象数组
+				 */
+				return arrivalFlightInfo;
+			}
+		}
+		return arrivalFlightInfo;
 	}
 }
