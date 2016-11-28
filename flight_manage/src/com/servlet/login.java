668f3b62e253b9e1ef89ff495d144c104a2a56b2
lww <1510825910@qq.com>
2016-11-28 21:39:46 +0800

    login servlet again

---
+++ flight_manage/src/com/servlet/login.java
@@ -0,0 +1,63 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
+
+import com.entity.Admin;
+import com.entity.Role;
+
+
+/**
+ * Servlet implementation class login
+ */
+//@WebServlet("/login")
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		processRequest(request,response);
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		processRequest(request,response);
+		
+	}
+	private void processRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		HttpSession session = request.getSession();
+		if(username.equals("aaa")&&password.equals("123456")){
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
+			
+		}else{
+			response.sendRedirect("login.jsp");;
+		}
+
+		
+	}
+
+}
