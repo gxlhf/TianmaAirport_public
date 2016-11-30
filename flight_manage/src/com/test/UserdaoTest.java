8356ac198446fbf65a1527d765f2005ac820f0b9
xxk1004 <374123548@qq.com>
2016-11-30 22:36:36 +0800

    与云端数据库对接，实现登陆后跳转到该管理员拥有的权限对应页面。

--- flight_manage/src/com/test/UserdaoTest.java
+++ flight_manage/src/com/test/UserdaoTest.java
@@ -1,18 +1,23 @@
 package com.test;
 
 import com.dao.LoginDao;
+import com.dao.RoleDao;
 import com.entity.Admin;
 
 public class UserdaoTest {
 	public static void main(String[] args){
 	   	 Admin ad= null;
-	        LoginDao lD=new LoginDao();
-	        ad=lD.loginCheck("1001", "888888");
-	        if(ad==null)
-	        {
-	        	System.out.println("null！");
-	        }
+	   	 RoleDao roleDao = new RoleDao();
+	     LoginDao lD=new LoginDao();
+	     ad=lD.loginCheck("9527", "19971004");
+	     if(ad==null)
+	     {
+	    	 System.out.println("null！");
+	     }
+	     //roleDao.roleDescriptionSearch(ad.getRole().getName());
+//	     System.out.println(ad.getRole().getName());
+//	     System.out.println(roleDao.roleDescriptionSearch("系统管理员"));
 	     System.out.println(ad.getEmpno()+ad.getName()+ad.getSex()+ad.getEmail()+ad.getDepartment()+ad.getPhone()+ad.getPosition()+ad.getMobile()+ad.getRole().getName()+ad.getRole().getDescription()+ad.getRole().getAuthorityMap());
 	        
-	    }
+	}
 }
