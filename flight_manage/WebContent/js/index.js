209be6a72e01bf4f3819aa077e928adc5f204b68
gxlhf <gx.lhf@163.com>
2016-12-05 00:53:32 +0800

    表单判断

--- flight_manage/WebContent/js/index.js
+++ flight_manage/WebContent/js/index.js
@@ -1,6 +1,16 @@
 $(window).resize(function () {
 	$('.item').height($(window).height() - $('nav').height());
 });
+$('#box-search button').click(function (e) {
+	e.preventDefault();
+	var flight_num_exp = /([a-z]{2}[0-9]{4})/i;
+	var form_elem = $(this).parent();
+	console.log(flight_num_exp.test(form_elem.find('[name = "key"]').val()));
+	// form_elem.submit();
+});
+$('#carousel').carousel({
+	keybord: false
+});
 $(function () {
 	$('.item').height($(window).height() - $('nav').height());
 });
