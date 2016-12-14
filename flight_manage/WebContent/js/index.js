a3a51952b806dae954e2bd32145af9eb5c4112b6
gxlhf <gx.lhf@163.com>
2016-12-15 00:40:21 +0800

    验证完成

--- flight_manage/WebContent/js/index.js
+++ flight_manage/WebContent/js/index.js
@@ -5,7 +5,7 @@ $('#box-search button').click(function (e) {
 	e.preventDefault();
 	var flight_num_exp = /([a-z]{2}[0-9]{4}|[a-z]{1}[0-9]{5}|[0-9]{1}[a-z]{1}[0-9]{4})/i;
 	var form_elem = $(this).parent();
-	var key_text = form_elem.find('[name = "key"]').val();
+	var key_text = form_elem.find('[name = "key"]').val().toUpperCase();
 	$('[name="is_flightNo"]').val(flight_num_exp.test(key_text));
 
 	form_elem.submit();
